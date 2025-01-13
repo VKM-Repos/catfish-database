985641ae7b186e9c6fb95eb9058d2f6551af9c43
salissadiq <salissadiq@gmail.com>
2025-01-13 14:46:23 +0100

    feat(admin-user): convert cluster manager ui

--- src/pages/dashboard/cluster-managers/index.tsx
+++ src/pages/dashboard/cluster-managers/index.tsx
@@ -1,19 +1,23 @@
 import { Container } from 'src/components/layouts/container'
-import { Center } from 'src/components/layouts/center'
 import PageTransition from 'src/components/animations/page-transition'
-import * as SolarIconSet from 'solar-icon-set'
 import { PageHeader } from 'src/components/layouts/page-header'
+import { ClusterManagerTable } from './components/cluster-manager-table'
+import { ImportDialog } from './components/import-dialog'
+import { AddClusterManagerFormDialog } from './components/add-cluster-manager-form-dialog'
 
 export default function ClusterManagers() {
   const title = 'Cluster Managers'
-  const actions = <></> // button goes here
+  const actions = (
+    <div className="mb-10 flex items-center justify-center gap-3">
+      <ImportDialog />
+      <AddClusterManagerFormDialog />
+    </div>
+  ) // button goes here
   return (
     <PageTransition>
       <Container>
         <PageHeader title={title} actions={actions} />
-        <Center>
-          <SolarIconSet.Database color="#1C274C" size={24} iconStyle="Outline" />
-        </Center>
+        <ClusterManagerTable />
       </Container>
     </PageTransition>
   )

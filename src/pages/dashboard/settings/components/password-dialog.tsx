44762f4f858f39d69d60bcacb2a43147981611a6
Susu-spec <suleimansuwaibat@gmail.com>
2025-01-17 12:22:26 +0100

    feat(settings-page): add account tab and features

---
+++ src/pages/dashboard/settings/components/password-dialog.tsx
@@ -0,0 +1,129 @@
+import { zodResolver } from '@hookform/resolvers/zod'
+import { useState } from 'react'
+import { useForm } from 'react-hook-form'
+import FormInput from 'src/components/form-input'
+import { FlexBox } from 'src/components/layouts/flexbox'
+import { Text } from 'src/components/layouts/text'
+import {
+  Dialog,
+  DialogTrigger,
+  DialogDescription,
+  DialogContent,
+  DialogClose,
+  DialogHeader,
+  DialogTitle,
+} from 'src/components/ui/dialog'
+import { Form } from 'src/components/ui/form'
+import { z } from 'zod'
+import { Button } from 'src/components/ui/button'
+
+const formSchema = z
+  .object({
+    old_password: z.string(),
+    new_password: z
+      .string()
+      .min(8, { message: 'Password must contain at least 8 characters' })
+      .refine((value) => /[A-Z]/.test(value), {
+        message: 'Password must contain at least one uppercase letter',
+      })
+      .refine((value) => /[a-z]/.test(value), {
+        message: 'Password must contain at least one lowercase letter',
+      })
+      .refine((value) => /\d/.test(value), {
+        message: 'Password must contain at least one number',
+      })
+      .refine((value) => /[!@#$%^&*(),.?":{}|<>_+\-=/[\]\\/~`']/.test(value), {
+        message: 'Password must contain at least one symbol',
+      }),
+    confirm_password: z.string(),
+  })
+  .refine((data) => data.new_password === data.confirm_password, {
+    message: 'Passwords do not match',
+    path: ['confirm_password'],
+  })
+
+type PasswordData = z.infer<typeof formSchema>
+
+export default function PasswordDialog() {
+  const [open, setOpen] = useState(false)
+
+  const form = useForm<PasswordData>({ resolver: zodResolver(formSchema) })
+  const {
+    formState: { isDirty },
+  } = form
+
+  const onSubmit = (data: PasswordData) => {
+    console.log(data)
+    setOpen(false)
+  }
+
+  return (
+    <>
+      <Dialog open={open} onOpenChange={setOpen}>
+        <DialogTrigger asChild>
+          <Button variant="outline" className="border-primary-400">
+            <Text className="text-primary-400">Change Password</Text>
+          </Button>
+        </DialogTrigger>
+        <DialogContent>
+          <div className="cursor-default bg-white">
+            <FlexBox direction="col" justify="center" gap="gap-6">
+              <DialogHeader className="w-full border-[0.5px] border-b-neutral-300 px-6 py-3">
+                <DialogTitle className="text-center text-xl font-bold">Change Password</DialogTitle>
+              </DialogHeader>
+              <DialogDescription />
+              <Form {...form}>
+                <form onSubmit={form.handleSubmit(onSubmit)} className="w-full">
+                  <FlexBox direction="col" gap="gap-6" justify="center" className="w-full p-6">
+                    <div className="w-full">
+                      <FormInput
+                        label="Old password"
+                        type="password"
+                        placeholder="Enter old password"
+                        className="!border-neutral-200"
+                        {...form.register('old_password')}
+                        disabled
+                      />
+                    </div>
+                    <div className="w-full">
+                      <FormInput
+                        label="New password"
+                        type="password"
+                        placeholder="Enter new password"
+                        className="!border-neutral-200"
+                        {...form.register('new_password')}
+                      />
+                    </div>
+                    <div className="w-full">
+                      <FormInput
+                        label="Confirm new password"
+                        type="password"
+                        placeholder="Repeat new password"
+                        {...form.register('confirm_password')}
+                        className="!border-neutral-200"
+                      />
+                    </div>
+                  </FlexBox>
+                  <footer
+                    className={`mx-auto w-full rounded-sm !bg-neutral-50 bg-neutral-100 !px-0 !py-0 px-8 py-4 text-center text-sm text-neutral-400`}
+                  >
+                    <FlexBox justify="between" align="center" className="px-6 py-3">
+                      <Button variant="outline">
+                        <DialogClose>
+                          <Text>Cancel</Text>
+                        </DialogClose>
+                      </Button>
+                      <Button type="submit" variant="primary" disabled={!isDirty}>
+                        <Text>Update Password</Text>
+                      </Button>
+                    </FlexBox>
+                  </footer>
+                </form>
+              </Form>
+            </FlexBox>
+          </div>
+        </DialogContent>
+      </Dialog>
+    </>
+  )
+}
---
+++ src/pages/dashboard/settings/components/profile-dialog.tsx
@@ -0,0 +1,137 @@
+import { zodResolver } from '@hookform/resolvers/zod'
+import { useState } from 'react'
+import { useForm } from 'react-hook-form'
+import FormInput from 'src/components/form-input'
+import { FlexBox } from 'src/components/layouts/flexbox'
+import { Text } from 'src/components/layouts/text'
+import {
+  Dialog,
+  DialogTrigger,
+  DialogContent,
+  DialogClose,
+  DialogHeader,
+  DialogTitle,
+  DialogDescription,
+} from 'src/components/ui/dialog'
+import { Form } from 'src/components/ui/form'
+import { Select, SelectContent, SelectGroup, SelectTrigger, SelectValue } from 'src/components/ui/select'
+import { z } from 'zod'
+import * as SolarIconSet from 'solar-icon-set'
+import { Button } from 'src/components/ui/button'
+import { Label } from 'src/components/ui/label'
+
+const formSchema = z.object({
+  first_name: z.string().min(1, { message: 'Please fill this field' }),
+  last_name: z.string().min(1, { message: 'Please fill this field' }),
+  email: z.string().email({ message: 'Invalid email address' }).min(1, { message: 'Please fill this field' }),
+  state: z.string().min(1, { message: 'Please fill this field' }),
+  phone_number: z.string().min(1, { message: 'Please fill this field' }),
+})
+
+type ProfileData = z.infer<typeof formSchema>
+
+export default function ProfileDialog() {
+  const [state, setState] = useState('Kaduna')
+  const [open, setOpen] = useState(false)
+
+  const form = useForm<ProfileData>({ resolver: zodResolver(formSchema) })
+  const {
+    formState: { isDirty },
+  } = form
+
+  const onSubmit = (data: ProfileData) => {
+    console.log(data)
+    setOpen(false)
+  }
+
+  return (
+    <>
+      <Dialog open={open} onOpenChange={setOpen}>
+        <DialogTrigger asChild>
+          <Button variant="outline" className="border-primary-400">
+            <FlexBox gap="gap-3" align="center">
+              <SolarIconSet.PenNewSquare color="#651391" size={20} iconStyle="Outline" />
+              <Text className="text-primary-400">Edit</Text>
+            </FlexBox>
+          </Button>
+        </DialogTrigger>
+        <DialogContent>
+          <div className="cursor-default bg-white">
+            <FlexBox direction="col" justify="center" gap="gap-6">
+              <DialogHeader className="w-full border-[0.5px] border-b-neutral-300 px-6 py-3">
+                <DialogTitle className="text-center text-xl font-bold">Edit Profile</DialogTitle>
+              </DialogHeader>
+              <DialogDescription />
+              <Form {...form}>
+                <form onSubmit={form.handleSubmit(onSubmit)}>
+                  <FlexBox direction="col" gap="gap-6" justify="center" className="w-full p-6">
+                    <FlexBox gap="gap-6" align="center">
+                      <FormInput
+                        label="First name"
+                        type="text"
+                        className="!border-neutral-200"
+                        {...form.register('first_name')}
+                      />
+                      <FormInput
+                        label="Last name"
+                        type="text"
+                        className="!border-neutral-200"
+                        {...form.register('last_name')}
+                      />
+                    </FlexBox>
+                    <div className="w-full">
+                      <FormInput
+                        label="Email"
+                        type="email"
+                        {...form.register('email')}
+                        className="!border-neutral-200"
+                      />
+                    </div>
+                    <div className="w-full">
+                      <Select>
+                        <FlexBox gap="gap-2" direction="col" justify="center">
+                          <Label>State</Label>
+                          <SelectTrigger className="w-full rounded border border-neutral-200 px-3 py-2 ">
+                            <FlexBox gap="gap-2" align="center" justify="center">
+                              <SolarIconSet.Signpost iconStyle="Outline" size={20} />
+                              <SelectValue className="w-full" placeholder={state} />
+                            </FlexBox>
+                          </SelectTrigger>
+                          <SelectContent>
+                            <SelectGroup></SelectGroup>
+                          </SelectContent>
+                        </FlexBox>
+                      </Select>
+                    </div>
+                    <div className="w-full">
+                      <FormInput
+                        label="Phone Number"
+                        type="text"
+                        className="!border-neutral-200"
+                        {...form.register('phone_number')}
+                      />
+                    </div>
+                  </FlexBox>
+                  <footer
+                    className={`mx-auto w-full rounded-sm !bg-neutral-50 bg-neutral-100 !px-0 !py-0 px-8 py-4 text-center text-sm text-neutral-400`}
+                  >
+                    <FlexBox justify="between" align="center" className="px-6 py-3">
+                      <Button variant="outline">
+                        <DialogClose>
+                          <Text>Back</Text>
+                        </DialogClose>
+                      </Button>
+                      <Button type="submit" variant="primary" disabled={!isDirty}>
+                        <Text>Edit Profile</Text>
+                      </Button>
+                    </FlexBox>
+                  </footer>
+                </form>
+              </Form>
+            </FlexBox>
+          </div>
+        </DialogContent>
+      </Dialog>
+    </>
+  )
+}

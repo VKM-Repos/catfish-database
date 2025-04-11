53b3b6cc307806a851d9ba634cb092dbab5dd279
Susu-spec <suleimansuwaibat@gmail.com>
2025-04-11 10:52:32 +0100

    fix(settings-forms): restructure password dialog

---
+++ src/pages/dashboard/settings/components/forms/password-form.tsx
@@ -0,0 +1,133 @@
+import { Heading } from 'src/components/ui/heading'
+import * as SolarIconSet from 'solar-icon-set'
+import { Form, FormControl, FormField, FormItem, FormMessage } from 'src/components/ui/form'
+import { Input } from 'src/components/ui/input'
+import { Alert, AlertTitle, AlertDescription } from 'src/components/ui/alert'
+import { Button } from 'src/components/ui/button'
+import { useState } from 'react'
+import { Label } from 'src/components/ui/label'
+
+export default function PasswordForm({ form, onSubmit, error, setOpen }: any) {
+  const { formState: isDirty } = form
+  const [showCurrentPassword, setShowCurrentPassword] = useState(false)
+  const [showNewPassword, setShowNewPassword] = useState(false)
+  const [showConfirmPassword, setShowConfirmPassword] = useState(false)
+
+  return (
+    <div className="w-full py-[4rem] pb-[6rem]">
+      <div className="absolute inset-x-0 top-0 w-full border-b border-b-neutral-200 py-2">
+        <Heading className="text-center" level={6}>
+          Change Password
+        </Heading>
+      </div>
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)} className="w-full space-y-8">
+          {error && (
+            <Alert variant="error" tone="filled">
+              <AlertTitle>{error.title}</AlertTitle>
+              <AlertDescription>{error.message}</AlertDescription>
+            </Alert>
+          )}
+          <FormField
+            control={form.control}
+            name="currentPassword"
+            render={({ field }) => (
+              <FormItem>
+                <FormControl>
+                  <div className="flex flex-col gap-2">
+                    <Label>Old password</Label>
+                    <Input
+                      placeholder="Current Password"
+                      type={showCurrentPassword ? 'text' : 'password'}
+                      {...field}
+                      icon={
+                        <Button
+                          variant="ghost"
+                          onClick={() => setShowCurrentPassword(!showCurrentPassword)}
+                          type="button"
+                          className="h-full w-full p-0"
+                        >
+                          {showCurrentPassword ? <SolarIconSet.EyeClosed size={20} /> : <SolarIconSet.Eye size={20} />}
+                        </Button>
+                      }
+                      iconPosition="right"
+                    />
+                  </div>
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="newPassword"
+            render={({ field }) => (
+              <FormItem>
+                <FormControl>
+                  <div className="flex flex-col gap-2">
+                    <Label>New password</Label>
+                    <Input
+                      placeholder="New Password"
+                      type={showNewPassword ? 'text' : 'password'}
+                      {...field}
+                      icon={
+                        <Button
+                          variant="ghost"
+                          onClick={() => setShowNewPassword(!showNewPassword)}
+                          type="button"
+                          className="h-full w-full p-0"
+                        >
+                          {showNewPassword ? <SolarIconSet.EyeClosed size={20} /> : <SolarIconSet.Eye size={20} />}
+                        </Button>
+                      }
+                      iconPosition="right"
+                    />
+                  </div>
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="confirmPassword"
+            render={({ field }) => (
+              <FormItem>
+                <FormControl>
+                  <div className="flex flex-col gap-2">
+                    <Label>Confirm new password</Label>
+                    <Input
+                      placeholder="Confirm New Password"
+                      type={showConfirmPassword ? 'text' : 'password'}
+                      {...field}
+                      icon={
+                        <Button
+                          variant="ghost"
+                          onClick={() => setShowConfirmPassword(!showConfirmPassword)}
+                          type="button"
+                          className="h-full w-full p-0"
+                        >
+                          {showConfirmPassword ? <SolarIconSet.EyeClosed size={20} /> : <SolarIconSet.Eye size={20} />}
+                        </Button>
+                      }
+                      iconPosition="right"
+                    />
+                  </div>
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <div className="absolute inset-x-0 bottom-0 mx-auto flex w-[98%] items-start justify-between rounded-md bg-neutral-50 p-3">
+            <Button variant="outline" type="button" onClick={() => setOpen(false)}>
+              Cancel
+            </Button>
+            <Button type="submit" variant="primary" disabled={!isDirty}>
+              Update Password
+            </Button>
+          </div>
+        </form>
+      </Form>
+    </div>
+  )
+}
--- src/pages/dashboard/settings/components/password-dialog.tsx
+++ src/pages/dashboard/settings/components/password-dialog.tsx
@@ -3,14 +3,10 @@ import { useState } from 'react'
 import { useForm } from 'react-hook-form'
 import { Text } from 'src/components/ui/text'
 import { Dialog, DialogTrigger, DialogContent } from 'src/components/ui/dialog'
-import { Form, FormControl, FormField, FormItem, FormMessage } from 'src/components/ui/form'
 import { z } from 'zod'
 import { Button } from 'src/components/ui/button'
-import { Input } from 'src/components/ui/input'
-import { Alert, AlertTitle, AlertDescription } from 'src/components/ui/alert'
-import { createPutMutationHook } from 'src/api/hooks/usePut'
-import { Heading } from 'src/components/ui/heading'
-import * as SolarIconSet from 'solar-icon-set'
+import { createPatchMutationHook } from 'src/api/hooks/usePatch'
+import PasswordForm from './forms/password-form'
 
 const baseSchema = z.object({
   currentPassword: z.string().min(1, { message: 'Current password is required' }),
@@ -46,21 +42,14 @@ export default function ChangePasswordDialog() {
   const [open, setOpen] = useState(false)
   const [step, setStep] = useState(1)
   const [error, setError] = useState<{ title: string; message: string } | null>(null)
-  const [showCurrentPassword, setShowCurrentPassword] = useState(false)
-  const [showNewPassword, setShowNewPassword] = useState(false)
-  const [showConfirmPassword, setShowConfirmPassword] = useState(false)
 
-  const updatePasswordMutation = createPutMutationHook({
-    endpoint: '/api/users/change-password',
+  const updatePasswordMutation = createPatchMutationHook({
+    endpoint: '/users/change-password',
     requestSchema: baseSchema,
-    responseSchema: z.object({}),
+    responseSchema: z.string(),
   })()
 
   const form = useForm<PasswordData>({ resolver: zodResolver(formSchema) })
-  const {
-    reset,
-    formState: { isDirty },
-  } = form
 
   const onSubmit = async (data: PasswordData) => {
     try {
@@ -83,133 +72,37 @@ export default function ChangePasswordDialog() {
     }
   }
 
-  const PasswordForm = () => (
-    <>
-      <div className="absolute inset-x-0 top-0 w-full border-b border-b-neutral-200 py-2">
-        <Heading className="text-center" level={6}>
-          Change Password
-        </Heading>
-      </div>
-      <Form {...form}>
-        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
-          {error && (
-            <Alert variant="error" tone="filled">
-              <AlertTitle>{error.title}</AlertTitle>
-              <AlertDescription>{error.message}</AlertDescription>
-            </Alert>
-          )}
-          <FormField
-            control={form.control}
-            name="currentPassword"
-            render={({ field }) => (
-              <FormItem>
-                <FormControl>
-                  <Input
-                    placeholder="Current Password"
-                    type={showCurrentPassword ? 'text' : 'password'}
-                    {...field}
-                    icon={
-                      <Button
-                        variant="ghost"
-                        onClick={() => setShowCurrentPassword(!showCurrentPassword)}
-                        type="button"
-                        className="h-full w-full p-0"
-                      >
-                        {showCurrentPassword ? <SolarIconSet.EyeClosed size={20} /> : <SolarIconSet.Eye size={20} />}
-                      </Button>
-                    }
-                    iconPosition="right"
-                  />
-                </FormControl>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
-          <FormField
-            control={form.control}
-            name="newPassword"
-            render={({ field }) => (
-              <FormItem>
-                <FormControl>
-                  <Input
-                    placeholder="New Password"
-                    type={showNewPassword ? 'text' : 'password'}
-                    {...field}
-                    icon={
-                      <Button
-                        variant="ghost"
-                        onClick={() => setShowNewPassword(!showNewPassword)}
-                        type="button"
-                        className="h-full w-full p-0"
-                      >
-                        {showNewPassword ? <SolarIconSet.EyeClosed size={20} /> : <SolarIconSet.Eye size={20} />}
-                      </Button>
-                    }
-                    iconPosition="right"
-                  />
-                </FormControl>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
-          <FormField
-            control={form.control}
-            name="confirmPassword"
-            render={({ field }) => (
-              <FormItem>
-                <FormControl>
-                  <Input
-                    placeholder="Confirm New Password"
-                    type={showConfirmPassword ? 'text' : 'password'}
-                    {...field}
-                    icon={
-                      <Button
-                        variant="ghost"
-                        onClick={() => setShowConfirmPassword(!showConfirmPassword)}
-                        type="button"
-                        className="h-full w-full p-0"
-                      >
-                        {showConfirmPassword ? <SolarIconSet.EyeClosed size={20} /> : <SolarIconSet.Eye size={20} />}
-                      </Button>
-                    }
-                    iconPosition="right"
-                  />
-                </FormControl>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
-          <div className="absolute inset-x-0 bottom-0 mx-auto flex w-[98%] items-start justify-between rounded-md bg-neutral-50 p-3">
-            <Button variant="outline" type="button" onClick={() => setOpen(false)}>
-              Cancel
-            </Button>
-            <Button type="submit" variant="primary" disabled={!isDirty}>
-              Update Password
+  const renderStep = () => {
+    switch (step) {
+      case 1:
+        return <PasswordForm form={form} onSubmit={onSubmit} error={error} setOpen={setOpen} />
+      case 2:
+        return (
+          <div className="my-8 flex w-full flex-col items-center justify-center gap-4">
+            <Text className="text-lg">Password changed successfully!</Text>
+            <Button
+              variant="primary"
+              onClick={() => {
+                setOpen(false)
+                setStep(1)
+                form.reset()
+              }}
+            >
+              Continue
             </Button>
           </div>
-        </form>
-      </Form>
-    </>
-  )
+        )
+      default:
+        return null
+    }
+  }
 
   return (
     <Dialog open={open} onOpenChange={setOpen}>
       <DialogTrigger asChild>
         <Button variant="outline">Change Password</Button>
       </DialogTrigger>
-      <DialogContent className="min-h-[410px] overflow-hidden px-8 py-4">
-        <div className="py-[4rem] pb-[6rem]">
-          {step === 1 && <PasswordForm />}
-          {step === 2 && (
-            <div className="flex h-[3rem] w-full flex-col items-center justify-center space-y-4">
-              <Text>Password changed successfully!</Text>
-              <Button variant="primary" onClick={() => setOpen(false)}>
-                Continue
-              </Button>
-            </div>
-          )}
-        </div>
-      </DialogContent>
+      <DialogContent className="max-h-fit w-full overflow-hidden px-8 py-4">{renderStep()}</DialogContent>
     </Dialog>
   )
 }

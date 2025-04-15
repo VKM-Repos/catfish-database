02b481cf8d3979ac00df8ce136c9e1cc2d0ab55d
Susu-spec <suleimansuwaibat@gmail.com>
2025-04-15 13:21:38 +0100

    feat(alert-error): handle empty string on errors array

--- src/components/global/form-error-alert/index.tsx
+++ src/components/global/form-error-alert/index.tsx
@@ -2,19 +2,20 @@ import { Alert, AlertDescription, AlertTitle } from 'src/components/ui/alert'
 import { ClientErrorType } from 'src/types'
 
 export default function FormValidationErrorAlert({ error }: { error: ClientErrorType }) {
-  const errorArray: React.ReactNode = Array.isArray(error.errors) ? (
-    <ul className="space-y-1">
-      {error.errors.map((e: string, i: number) => {
-        const [field, ...rest] = e.split(':')
+  const errorArray: React.ReactNode =
+    Array.isArray(error.errors) && error.errors.toString() !== '' ? (
+      <ul className="space-y-1">
+        {error.errors.map((e: string, i: number) => {
+          const [field, ...rest] = e.split(':')
 
-        return (
-          <li key={i} className="my-1">
-            <strong>{field.trim().charAt(0).toUpperCase() + field.trim().slice(1)}:</strong> {rest.join(':').trim()}
-          </li>
-        )
-      })}
-    </ul>
-  ) : null
+          return (
+            <li key={i} className="my-1">
+              <strong>{field.trim().charAt(0).toUpperCase() + field.trim().slice(1)}:</strong> {rest.join(':').trim()}
+            </li>
+          )
+        })}
+      </ul>
+    ) : null
 
   return (
     <Alert variant="error" tone="filled">

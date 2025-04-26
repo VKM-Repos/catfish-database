20bc7b24450ad349741fa80df79ca61ec20a636d
Susu-spec <suleimansuwaibat@gmail.com>
2025-04-26 20:37:34 +0100

    feat(ponds-page): modify `AddFishPond` structure

--- src/pages/dashboard/ponds/components/add-fish-to-pond/index.tsx
+++ src/pages/dashboard/ponds/components/add-fish-to-pond/index.tsx
@@ -3,6 +3,7 @@ import { useForm } from 'react-hook-form'
 import { Button } from 'src/components/ui/button'
 import { FlexBox } from 'src/components/ui/flexbox'
 import { Form, FormControl, FormField, FormItem, FormMessage } from 'src/components/ui/form'
+import { Select, SelectTrigger, SelectValue, SelectContent, SelectItem } from 'src/components/ui/select'
 import { Input } from 'src/components/ui/input'
 import { Text } from 'src/components/ui/text'
 import { fishDetailsSchema } from 'src/schemas'
@@ -11,6 +12,7 @@ import { z } from 'zod'
 
 export default function AddFishPond({ handleNext }: { handleNext: () => void }) {
   const { pondData, setPondStore } = usePondStore()
+  const ponds = ['Pond 1A', 'Pond 1B', 'Pond 2A', 'Pond 2B']
 
   const form = useForm({
     resolver: zodResolver(fishDetailsSchema),
@@ -51,7 +53,7 @@ export default function AddFishPond({ handleNext }: { handleNext: () => void }) 
         <form onSubmit={form.handleSubmit(onSubmit)} className="flex w-full flex-col items-center space-y-8">
           <FlexBox gap="gap-5" direction="col" align="center" className="w-full">
             <div className="flex w-full flex-col gap-2">
-              <Text className="text-sm font-medium text-neutral-300">
+              <Text className="text-sm font-medium text-neutral-600">
                 Select the pond to which you want to add the field
               </Text>
               <FormField
@@ -60,7 +62,24 @@ export default function AddFishPond({ handleNext }: { handleNext: () => void }) 
                 render={({ field }) => (
                   <FormItem>
                     <FormControl>
-                      <Input placeholder="e.g Pond 1A" {...field} />
+                      <Select
+                        value={field.value ? String(field.value) : ''}
+                        onValueChange={(value) => field.onChange(value)}
+                        defaultValue={field.value}
+                      >
+                        <SelectTrigger className="font-light">
+                          <div className="flex items-center justify-center gap-2">
+                            <SelectValue placeholder="Select pond" />
+                          </div>
+                        </SelectTrigger>
+                        <SelectContent>
+                          {ponds?.map((pond, index) => (
+                            <SelectItem key={index} value={pond}>
+                              {pond}
+                            </SelectItem>
+                          ))}
+                        </SelectContent>
+                      </Select>
                     </FormControl>
                     <FormMessage />
                   </FormItem>
@@ -68,14 +87,14 @@ export default function AddFishPond({ handleNext }: { handleNext: () => void }) 
               />
             </div>
             <div className="flex w-full flex-col gap-2">
-              <Text className="text-sm font-medium text-neutral-300">Quantity of fish supplied</Text>
+              <Text className="text-sm font-medium text-neutral-600">Quantity of fish supplied</Text>
               <FormField
                 control={form.control}
                 name="fishQuantity"
                 render={({ field }) => (
                   <FormItem>
                     <FormControl>
-                      <Input placeholder="e.g Pond 1A" {...field} />
+                      <Input placeholder="Input number of fish added to pond" {...field} type="number" />
                     </FormControl>
                     <FormMessage />
                   </FormItem>
@@ -83,14 +102,14 @@ export default function AddFishPond({ handleNext }: { handleNext: () => void }) 
               />
             </div>
             <div className="flex w-full flex-col gap-2">
-              <Text className="text-sm font-medium text-neutral-300">Fish Size</Text>
+              <Text className="text-sm font-medium text-neutral-600">Fish Size</Text>
               <FormField
                 control={form.control}
                 name="fishSize"
                 render={({ field }) => (
                   <FormItem>
                     <FormControl>
-                      <Input placeholder="e.g Pond 1A" {...field} type="number" />
+                      <Input placeholder="Input size of fish" {...field} type="number" />
                     </FormControl>
                     <FormMessage />
                   </FormItem>
@@ -98,14 +117,14 @@ export default function AddFishPond({ handleNext }: { handleNext: () => void }) 
               />
             </div>
             <div className="flex w-full flex-col gap-2">
-              <Text className="text-sm font-medium text-neutral-300">Fish Description</Text>
+              <Text className="text-sm font-medium text-neutral-600">Fish Description</Text>
               <FormField
                 control={form.control}
                 name="fishDescription"
                 render={({ field }) => (
                   <FormItem>
                     <FormControl>
-                      <Input placeholder="e.g Pond 1A" {...field} />
+                      <Input placeholder="Input description of fish added to pond" {...field} />
                     </FormControl>
                     <FormMessage />
                   </FormItem>
@@ -113,14 +132,23 @@ export default function AddFishPond({ handleNext }: { handleNext: () => void }) 
               />
             </div>
             <div className="flex w-full flex-col gap-2">
-              <Text className="text-sm font-medium text-neutral-300">Cost of fish</Text>
+              <Text className="text-sm font-medium text-neutral-600">Cost of fish</Text>
               <FormField
                 control={form.control}
                 name="fishCost"
                 render={({ field }) => (
                   <FormItem>
                     <FormControl>
-                      <Input placeholder="e.g Pond 1A" {...field} />
+                      <div className="focus-within:ring-offset-background flex max-h-fit items-center rounded-md border border-neutral-200 focus-within:ring-2 focus-within:ring-primary-500 focus-within:ring-offset-2">
+                        <div className="h-full bg-neutral-100 px-3 py-[.65rem] text-sm text-neutral-500">₦</div>
+                        <div className="w-full">
+                          <Input
+                            placeholder="Amount in naira"
+                            {...field}
+                            className="!w-full border-0 px-3 text-sm focus-visible:ring-0 focus-visible:ring-offset-0"
+                          />
+                        </div>
+                      </div>
                     </FormControl>
                     <FormMessage />
                   </FormItem>

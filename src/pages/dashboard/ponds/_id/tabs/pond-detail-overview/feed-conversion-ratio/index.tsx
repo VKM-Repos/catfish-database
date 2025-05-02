3350af460414a09e16ead03f2b58eb54f923c355
Susu-spec <suleimansuwaibat@gmail.com>
2025-05-02 11:52:36 +0100

    feat(pond-details): add content for pond details overview components

---
+++ src/pages/dashboard/ponds/_id/tabs/pond-detail-overview/feed-conversion-ratio/index.tsx
@@ -0,0 +1,60 @@
+import GaugeChart from 'react-gauge-chart'
+
+export default function FeedConversionRatio() {
+  const value = 0.98
+
+  function roundToHalf(num: number): number {
+    return Math.round(num * 2) / 2
+  }
+
+  function calculateRange(value: number) {
+    let min, mid, max
+    if (value <= 1) {
+      min = 0.25
+      mid = 0.5
+      max = 1
+    } else {
+      min = roundToHalf(value - 0.5)
+      mid = roundToHalf(value)
+      max = roundToHalf(mid + 0.5)
+    }
+
+    if (min === mid) {
+      mid = roundToHalf(mid + 0.5)
+    }
+
+    if (mid === max) {
+      max = roundToHalf(max + 0.5)
+    }
+
+    return { min, mid, max }
+  }
+
+  const range = calculateRange(value)
+
+  return (
+    <div className="mb-4 flex min-h-full w-full flex-col gap-12 rounded-[.875rem] border border-[#8686871A] p-4 shadow-lg md:w-[45%]">
+      <div className="flex items-center justify-between py-4">
+        <p className="text-sm font-semibold">Feed Conversion Ratio</p>
+      </div>
+
+      <div className="flex flex-col justify-between">
+        <GaugeChart
+          className="min-h-fit"
+          nrOfLevels={420}
+          arcsLength={[1.5, 2.0, 1.5]}
+          colors={['#FAC898', '#FF5F15', '#CC5500']}
+          percent={value}
+          arcPadding={0.01}
+          formatTextValue={(value: string) => (Number(value) / 100).toString()}
+          textColor="#000"
+        />
+        <p className="mx-auto flex w-full max-w-[70%] items-center justify-between">
+          {Object.entries(range).map(([key, val]) => (
+            <span key={key}>{val.toFixed(1)}</span>
+          ))}
+        </p>
+      </div>
+    </div>
+  )
+}
---
+++ src/pages/dashboard/ponds/_id/tabs/pond-detail-overview/monthly-feed-consumption/index.tsx
@@ -0,0 +1,84 @@
+import { Bar, BarChart, CartesianGrid, XAxis, YAxis } from 'recharts'
+import { ChartContainer } from 'src/components/ui/chart'
+
+const monthlyFeedConfig = {
+  monthlyFeed: {
+    label: '',
+    color: '#651391B2',
+  },
+}
+
+export default function MonthlyFeedConsumption() {
+  return (
+    <div className="mb-4  w-full rounded-[.875rem] border border-[#8686871A] p-4 shadow-lg">
+      <div className="flex items-center justify-between py-4">
+        <p className="text-sm font-semibold">Monthly Feed Consumption (kg)</p>
+      </div>
+      <ChartContainer config={monthlyFeedConfig}>
+        <BarChart accessibilityLayer data={monthlyFeedData} height={100} barCategoryGap={14}>
+          <CartesianGrid horizontal={false} vertical={false} />
+          <XAxis
+            dataKey="month"
+            tickLine={false}
+            tickMargin={10}
+            axisLine={false}
+            tickFormatter={(value) => value.slice(0, 3)}
+          />
+          <YAxis tick={false} tickLine={false} axisLine={false} />
+          <Bar dataKey="monthlyFeed" fill="#651391" radius={5} />
+        </BarChart>
+      </ChartContainer>
+    </div>
+  )
+}
+
+const monthlyFeedData = [
+  {
+    month: 'January',
+    monthlyFeed: '50',
+  },
+  {
+    month: 'February',
+    monthlyFeed: '40',
+  },
+  {
+    month: 'March',
+    monthlyFeed: '30',
+  },
+  {
+    month: 'April',
+    monthlyFeed: '20',
+  },
+  {
+    month: 'May',
+    monthlyFeed: '10',
+  },
+  {
+    month: 'June',
+    monthlyFeed: '60',
+  },
+  {
+    month: 'July',
+    monthlyFeed: '90',
+  },
+  {
+    month: 'August',
+    monthlyFeed: '70',
+  },
+  {
+    month: 'September',
+    monthlyFeed: '70',
+  },
+  {
+    month: 'October',
+    monthlyFeed: '30',
+  },
+  {
+    month: 'November',
+    monthlyFeed: '50',
+  },
+  {
+    month: 'December',
+    monthlyFeed: '10',
+  },
+]

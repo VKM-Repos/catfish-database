9a8f157c8ad7f69c6c00ae1fb99fa9d7be2f4cd0
Susu-spec <suleimansuwaibat@gmail.com>
2025-05-07 16:59:20 +0100

    feat(ponds-table): add ponds table

--- src/pages/dashboard/ponds/components/ponds-table/index.tsx
+++ src/pages/dashboard/ponds/components/ponds-table/index.tsx
@@ -4,67 +4,37 @@ import EmptyTableState from 'src/components/global/empty-state'
 import { useNavigate } from 'react-router-dom'
 import EmptyClusterManagerImg from 'src/assets/images/empty-cluster-manager.jpg'
 import { paths } from 'src/routes'
+import { createGetQueryHook } from 'src/api/hooks/useGet'
+import { paginatedFishBatchResponseSchema } from 'src/schemas'
 
 export function PondsTable() {
   const isLoading = false
   const navigate = useNavigate()
 
   const openCreateModal = () => {
-    navigate(paths.dashboard.ponds.create)
+    navigate(paths.dashboard.ponds.create.addPond)
   }
+
+  const useGetFishBatches = createGetQueryHook({
+    endpoint: '/fish-batches',
+    responseSchema: paginatedFishBatchResponseSchema,
+    queryKey: ['fish-batches'],
+  })
+
+  const { data: fishBatches } = useGetFishBatches()
 
   return (
     <>
-      {ponds && ponds.length < 0 ? (
-        <DataTable columns={columns} data={ponds ?? []} isLoading={isLoading} emptyStateMessage="No ponds found" />
+      {fishBatches && fishBatches?.content.length > 0 ? (
+        <DataTable
+          columns={columns}
+          data={fishBatches?.content ?? []}
+          isLoading={isLoading}
+          emptyStateMessage="No ponds found"
+        />
       ) : (
         <EmptyTableState image={EmptyClusterManagerImg} name="pond" text="a pond" buttonFunc={openCreateModal} />
       )}
     </>
   )
 }
-
-export const ponds = [
-  {
-    pondName: 'Pond 1A',
-    fishQuantity: '1,800',
-    weight: '320g',
-    lastSampled: '02/04/2025',
-    pondStatus: true,
-  },
-  {
-    pondName: 'Pond 1A',
-    fishQuantity: '1,800',
-    weight: '320g',
-    lastSampled: '02/04/2025',
-    pondStatus: true,
-  },
-  {
-    pondName: 'Pond 1A',
-    fishQuantity: '1,800',
-    weight: '320g',
-    lastSampled: '02/04/2025',
-    pondStatus: true,
-  },
-  {
-    pondName: 'Pond 1A',
-    fishQuantity: '1,800',
-    weight: '320g',
-    lastSampled: '02/04/2025',
-    pondStatus: true,
-  },
-  {
-    pondName: 'Pond 1A',
-    fishQuantity: '1,800',
-    weight: '320g',
-    lastSampled: '02/04/2025',
-    pondStatus: true,
-  },
-  {
-    pondName: 'Pond 1A',
-    fishQuantity: '1,800',
-    weight: '320g',
-    lastSampled: '02/04/2025',
-    pondStatus: true,
-  },
-]

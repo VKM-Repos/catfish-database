3116872e5d46f3c504fc8f5c8954772c7b124278
salissadiq <salissadiq@gmail.com>
2025-05-14 13:59:19 +0100

    feat(table-action): create table action dropdown for all reports

---
+++ src/pages/dashboard/reports/components/actions-dropdown.tsx
@@ -0,0 +1,49 @@
+import { MoreHorizontal } from 'lucide-react'
+import { Button } from 'src/components/ui/button'
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from 'src/components/ui/dropdown-menu'
+import { useNavigate } from 'react-router-dom'
+import { paths } from 'src/routes/paths'
+// import { DeactivateUserDialog } from '../modals/deactivate-user'
+import { useState } from 'react'
+import type { User } from 'src/types'
+import * as SolarIconSet from 'solar-icon-set'
+
+type ActionsDropdownProps = {
+  user: User
+}
+
+export function ReportActionsDropdown({ user }: ActionsDropdownProps) {
+  const navigate = useNavigate()
+  const [menu, setMenu] = useState(false)
+
+  return (
+    <>
+      <DropdownMenu>
+        <DropdownMenuTrigger asChild>
+          <Button variant="ghost" className="h-8 w-8 p-0">
+            <span className="sr-only">Open menu</span>
+            <MoreHorizontal className="h-4 w-4" />
+          </Button>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent align="start">
+          <DropdownMenuItem onClick={() => navigate(paths.dashboard.farmers.view(user.id))}>
+            <SolarIconSet.Eye /> View
+          </DropdownMenuItem>
+          <DropdownMenuItem onClick={() => navigate(paths.dashboard.farmers.id(user.id))}>
+            <SolarIconSet.Pen2 /> Edit
+          </DropdownMenuItem>
+          <DropdownMenuItem onSelect={() => setMenu(true)}>
+            <SolarIconSet.TrashBin2 /> Delete
+          </DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
+
+      {/* <DeactivateUserDialog user={user} open={menu} onOpenChange={setMenu} /> */}
+    </>
+  )
+}

789342a0e584e462f050ca9deb81efdb5f746d32
salissadiq <salissadiq@gmail.com>
2025-05-18 21:12:17 +0100

    refactor(sampling-data): add sampling data for demo

--- src/pages/dashboard/reports/tabs/reports-table/harvest-records/index.tsx
+++ src/pages/dashboard/reports/tabs/reports-table/harvest-records/index.tsx
@@ -2,7 +2,6 @@ import { DataTable } from 'src/components/ui/data-table'
 import { columns } from './columns'
 import { FlexBox } from 'src/components/ui/flexbox'
 import { Text } from 'src/components/ui/text'
-import { samplingHistory } from '../sampling-history'
 
 export default function HarvestReportsTable() {
   return (
@@ -20,3 +19,78 @@ export default function HarvestReportsTable() {
     </FlexBox>
   )
 }
+
+export const samplingHistory = [
+  {
+    date: '02/04/2025',
+    fishPopulation: '2500',
+    sampleSize: '30',
+    averageWeight: '340g',
+    averageWeightGain: '1.5mm',
+    mortality: '5',
+    splitTriggered: true,
+    fishMoved: '500',
+    destinationPond: 'Back Tank',
+    reason: 'Harvest',
+  },
+  {
+    date: '02/04/2025',
+    fishPopulation: '2500',
+    sampleSize: '30',
+    averageWeight: '340g',
+    averageWeightGain: '1.5mm',
+    mortality: '5',
+    splitTriggered: true,
+    fishMoved: '500',
+    destinationPond: 'Back Tank',
+    reason: 'Transfer',
+  },
+  {
+    date: '02/04/2025',
+    fishPopulation: '2500',
+    sampleSize: '30',
+    averageWeight: '340g',
+    averageWeightGain: '1.5mm',
+    mortality: '5',
+    splitTriggered: false,
+    fishMoved: '500',
+    destinationPond: 'Back Tank',
+    reason: '-',
+  },
+  {
+    date: '02/04/2025',
+    fishPopulation: '2500',
+    sampleSize: '30',
+    averageWeight: '340g',
+    averageWeightGain: '1.5mm',
+    mortality: '5',
+    splitTriggered: false,
+    fishMoved: '500',
+    destinationPond: 'Back Tank',
+    reason: '-',
+  },
+  {
+    date: '02/04/2025',
+    fishPopulation: '2500',
+    sampleSize: '30',
+    averageWeight: '340g',
+    averageWeightGain: '1.5mm',
+    mortality: '5',
+    splitTriggered: false,
+    fishMoved: '500',
+    destinationPond: 'Back Tank',
+    reason: '-',
+  },
+  {
+    date: '02/04/2025',
+    fishPopulation: '2500',
+    sampleSize: '30',
+    averageWeight: '340g',
+    averageWeightGain: '1.5mm',
+    mortality: '5',
+    splitTriggered: false,
+    fishMoved: '500',
+    destinationPond: 'Back Tank',
+    reason: '-',
+  },
+]

2ae42aaac2ce79d1e153cbe0c75303f807a9399e
Susu-spec <suleimansuwaibat@gmail.com>
2025-05-30 16:14:57 +0100

    feat(feed-type): revert change

--- src/pages/dashboard/home/get-started/index.tsx
+++ src/pages/dashboard/home/get-started/index.tsx
@@ -237,7 +237,7 @@ export default function GetStarted() {
       </div>
 
       <div className="mt-8 flex flex-wrap justify-center gap-5">
-        {hasPondAndBatch ? (
+        {!hasPondAndBatch ? (
           <UnregisteredPondAndBatchCards navigate={navigate} hasPond={hasPond} />
         ) : (
           <RegisteredPondAndBatchCards navigate={navigate} />

510635ab6e60d8f09a044c42b0b461d4524540f5
Jawn <jawnchuks@gmail.com>
2025-06-05 10:47:43 +0100

    feat(create-report-dialog): add CreateReportDialog component for report submission

---
+++ src/pages/dashboard/inventory/components/modals/create-report-modal.tsx
@@ -0,0 +1,40 @@
+import { useNavigate } from 'react-router-dom'
+import { Button } from 'src/components/ui/button'
+import { Dialog, DialogContent } from 'src/components/ui/dialog'
+import { paths } from 'src/routes'
+
+type CreateReportDialogProps = {
+  open: boolean
+  onOpenChange: (open: boolean) => void
+  resetForm: () => void
+}
+
+export function CreateReportDialog({ open, onOpenChange, resetForm }: CreateReportDialogProps) {
+  const navigate = useNavigate()
+
+  const handleCancel = () => {
+    onOpenChange(false)
+    navigate(paths.dashboard.home.getStarted)
+    resetForm()
+  }
+  const addReport = () => {
+    onOpenChange(false)
+    resetForm()
+  }
+  return (
+    <Dialog open={open}>
+      <DialogContent className="overflow-hidden p-8">
+        <div className="my-5">
+          <h2 className="text-center text-[2rem] font-semibold">Recorded!</h2>
+          <p className="text-center">
+            Your report for today has been captured, do you want to submit another one today?
+          </p>
+        </div>
+        <Button onClick={addReport}>Yes, submit another report</Button>
+        <Button type="button" onClick={handleCancel} variant="link" className="font-semibold text-primary-500">
+          No , I will do this later
+        </Button>
+      </DialogContent>
+    </Dialog>
+  )
+}

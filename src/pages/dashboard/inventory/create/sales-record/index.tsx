d78e2a9d9ccf7a7b1d6d15410fc5d0381bb17b6d
Jawn <jawnchuks@gmail.com>
2025-06-06 12:42:15 +0100

    feat(sales-record): implement AddSalesRecord component with step navigation

--- src/pages/dashboard/inventory/create/sales-record/index.tsx
+++ src/pages/dashboard/inventory/create/sales-record/index.tsx
@@ -1,5 +1,66 @@
-import React from 'react'
+import { useLocation, useNavigate } from 'react-router-dom'
+import { Dialog, DialogContent } from 'src/components/ui/dialog'
+import { paths } from 'src/routes/paths'
+import { useState } from 'react'
+import { Text } from 'src/components/ui/text'
+import { Button } from 'src/components/ui/button'
+import { Heading } from 'src/components/ui/heading'
+import SalesRecordsForm from './sales-record-form'
 
-export default function DailyFarmRepo() {
-  return <div>Create Daily farm report</div>
+export default function AddSalesRecord() {
+  const navigate = useNavigate()
+  const [step, setStep] = useState(1)
+  const location = useLocation()
+
+  const RenderSteps = () => {
+    switch (step) {
+      case 1:
+        return (
+          <>
+            <div className="absolute inset-x-0 top-0 w-full border-b border-b-neutral-200 py-2">
+              <Heading className="text-center" level={6}>
+                {location.state ? `Edit sales record` : 'Add sales record'}
+              </Heading>
+            </div>
+            <SalesRecordsForm
+              mode="create"
+              setStep={setStep}
+              onCancel={() => navigate(`${paths.dashboard.inventory.root}?tab=sales-records`)}
+            />
+          </>
+        )
+      case 2:
+        return (
+          <div className="my-8 flex w-full flex-col items-center justify-center gap-4">
+            <Text className="text-lg font-semibold">Sales report updated successfully!</Text>
+            <Button
+              variant="primary"
+              onClick={() => {
+                navigate(`${paths.dashboard.inventory.root}?tab=sales-records`)
+                setTimeout(() => {
+                  setStep(1)
+                }, 1000)
+              }}
+            >
+              Continue
+            </Button>
+          </div>
+        )
+      default:
+        return null
+    }
+  }
+
+  return (
+    <Dialog open={true} onOpenChange={() => navigate(`${paths.dashboard.inventory.root}?tab=sales-records`)}>
+      <DialogContent
+        onInteractOutside={(e) => {
+          e.preventDefault()
+        }}
+        className="max-h-[80vh] max-w-[750px] overflow-y-scroll p-8"
+      >
+        <RenderSteps />
+      </DialogContent>
+    </Dialog>
+  )
 }

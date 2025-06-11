15e101a5f8534f4edbe1fbd1707e38c5cc8f1bc4
Jawn <jawnchuks@gmail.com>
2025-06-11 18:20:36 +0100

    fix(permissions-saved-modal): update modal to accept props for open state and close handler

--- src/pages/dashboard/system/permissions/components/modals/permissions-saved.tsx
+++ src/pages/dashboard/system/permissions/components/modals/permissions-saved.tsx
@@ -3,16 +3,18 @@ import { Text } from 'src/components/ui/text'
 import { Button } from 'src/components/ui/button'
 import { Heading } from 'src/components/ui/heading'
 
-export default function PermissionsSavedModal() {
+export default function PermissionsSavedModal({ open, onClose }: { open: boolean; onClose: () => void }) {
   return (
-    <Dialog open={false}>
+    <Dialog open={open} onOpenChange={onClose}>
       <DialogContent className="max-w-[478px] overflow-hidden p-8">
-        <div className="flex h-[3rem] w-full flex-col items-center justify-center space-y-4">
+        <div className="flex w-full flex-col items-center justify-center space-y-4">
           <Heading level={6}>Completed!</Heading>
           <Text weight="light" size="base">
             Permissions updated successfully!
           </Text>
-          <Button variant="primary">Continue</Button>
+          <Button variant="primary" onClick={onClose}>
+            Continue
+          </Button>
         </div>
       </DialogContent>
     </Dialog>

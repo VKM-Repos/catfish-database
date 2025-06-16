23d11ffae5403decfa576d3fd3a62a02cbf41d36
salissadiq <salissadiq@gmail.com>
2025-06-16 11:57:47 +0100

    fix(router): update path for creating feed stock to use dynamic routing

--- src/routes/router.tsx
+++ src/routes/router.tsx
@@ -349,7 +349,7 @@ export const router = createBrowserRouter([
             ],
           },
           {
-            path: 'create/feed-stock',
+            path: paths.dashboard.inventory.createFeedStock(':from'),
             element: (
               <RoleGuard allowedRoles={[UserRole.FARMER]}>
                 {LazyPage(() => import('src/pages/dashboard/inventory/create/feed-stock'))}

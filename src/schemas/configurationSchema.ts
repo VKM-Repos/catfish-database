0202a2ed8133c64da692fc0a216d842499cdf067
Laouali <laoualibachir2000@gmail.com>
2025-06-30 16:11:26 +0100

    feat(confi schema): define configuration schema and type

---
+++ src/schemas/configurationSchema.ts
@@ -0,0 +1,21 @@
+import { z } from 'zod'
+
+export const configSchema = z.object({
+  id: z.string().uuid(),
+  key: z.string(),
+  value: z.string(),
+  listValue: z.array(z.string()),
+  description: z.string(),
+  createdAt: z.string(),
+  updatedAt: z.string(),
+})
+
+export const paginatedConfigResponseSchema = z.object({
+  totalPages: z.number(),
+  totalElements: z.number(),
+  page: z.number(),
+  size: z.number(),
+  content: z.array(configSchema),
+})
+
+export const configResponseSchema = configSchema
---
+++ src/types/config.types.ts
@@ -0,0 +1,9 @@
+export type Config = {
+  id: string
+  key: string
+  value: string
+  listValue: string[]
+  description: string
+  createdAt: string
+  updatedAt: string
+}

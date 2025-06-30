342a1fc5922cdedfa24e051b4214f8c3724a7aeb
salissadiq <salissadiq@gmail.com>
2025-06-30 14:27:20 +0100

    feat(feed-insight): enhance feed insights chart with dynamic data and price summary

--- src/pages/dashboard/home/overview/components/super-admin/feed-insight.tsx
+++ src/pages/dashboard/home/overview/components/super-admin/feed-insight.tsx
@@ -1,69 +1,85 @@
-'use client'
-
-import { Bar, BarChart, CartesianGrid, XAxis, YAxis } from 'recharts'
-
-import { Card, CardContent } from 'src/components/ui/card'
-import { ChartConfig, ChartContainer, ChartTooltip, ChartTooltipContent } from 'src/components/ui/chart'
+import { useState } from 'react'
+import { Bar, BarChart, CartesianGrid, LabelList, XAxis, YAxis } from 'recharts'
+import { createGetQueryHook } from 'src/api/hooks/useGet'
+import { Card } from 'src/components/ui/card'
+import { ChartContainer, ChartTooltip, ChartTooltipContent } from 'src/components/ui/chart'
 import { FlexBox } from 'src/components/ui/flexbox'
 import { Text } from 'src/components/ui/text'
+import { z } from 'zod'
 
-export const description = 'A bar chart'
-
-const chartData = [
-  { month: 'Coppens', desktop: 186 },
-  { month: 'Vitals', desktop: 305 },
-  { month: 'Topfeed', desktop: 237 },
-  { month: 'Blue crown', desktop: 73 },
-]
-
-const chartConfig = {
-  desktop: {
-    label: 'Value',
-    color: '#651391',
+const monthlyFeedConfig = {
+  monthlyFeed: {
+    label: '',
+    color: '#651391B2',
   },
-} satisfies ChartConfig
+}
 
-export function FeedInsight() {
+export default function FeedInsight() {
+  const [selectedFeedType, setSelectedFeedType] = useState<string | null>(null)
+
+  const useGetFeedConsumed = createGetQueryHook({
+    endpoint: '/dashboards/super-admin/feed-consumption-trend?interval=ALL',
+    responseSchema: z.any(),
+    queryKey: ['most-feed-consumed-trend-super-admin'],
+  })
+  const { data: feedConsumed } = useGetFeedConsumed()
+
+  const useGetFeedPriceSummary = createGetQueryHook({
+    endpoint: '/dashboards/super-admin/feed-price-summary',
+    responseSchema: z.any(),
+    queryKey: ['feed-price-summary-summary-super-admin'],
+  })
+  const { data: feedPriceSummary } = useGetFeedPriceSummary()
+  console.log(feedPriceSummary)
+
+  const restructuredData = feedConsumed?.map((item: any) => ({
+    feedType: item.feedType,
+    quantityKg: item.dataPoints[0].quantityInKg,
+  }))
+
   return (
-    <Card className="h-[630px] max-h-[630px] min-h-[630px] w-full border border-neutral-200 p-[16px]">
-      <CardContent>
-        <Text className="mb-[20px]  font-semibold">Feed Insights</Text>
-        <Text className="mb-[10px] text-[14px]">Most used feed brands (%)</Text>
-        <ChartContainer config={chartConfig}>
-          <BarChart accessibilityLayer data={chartData}>
-            <CartesianGrid vertical={true} />
-            <XAxis dataKey="month" tickLine={false} tickMargin={10} axisLine={false} />
-            <YAxis
-              tick={{ fill: '#737780', fontSize: 10 }}
-              axisLine={false}
-              tickLine={false}
-              tickMargin={4}
-              width={20}
-              max={100}
-              min={0}
+    <Card className="h-[640px] max-h-[640px] w-full rounded-[.875rem] border border-neutral-200 p-4">
+      <Text className="mb-[46px] text-[14px] font-semibold">Feed Insights</Text>
+      <Text className="text-[14px] font-medium">Most used feed brands (%)</Text>
+      <ChartContainer className="-ml-5 mt-[10px] w-full" config={monthlyFeedConfig}>
+        <BarChart accessibilityLayer data={restructuredData} height={100} barCategoryGap={20}>
+          <CartesianGrid />
+          <XAxis dataKey="feedType" tickLine={false} tickMargin={10} axisLine={false} />
+          <YAxis tick={false} tickLine={false} axisLine={false} />
+          <YAxis tick={{ fill: '#737780', fontSize: 10 }} axisLine={false} tickLine={false} tickMargin={4} width={90} />
+          <ChartTooltip
+            cursor={false}
+            content={<ChartTooltipContent indicator="dot" hideLabel={false} className="!min-w-[10rem] bg-white !p-2" />}
+          />
+          <Bar dataKey="quantityKg" fill="#651391" radius={5}>
+            <LabelList
+              dataKey="quantityKg"
+              position="insideTop"
+              offset={12}
+              className="fill-white"
+              fontSize={14}
+              formatter={(quantityKg: string) => `${quantityKg}%`}
             />
-            <ChartTooltip cursor={false} content={<ChartTooltipContent className="bg-white" hideLabel />} />
-            <Bar dataKey="desktop" fill="var(--color-desktop)" radius={8} />
-          </BarChart>
-        </ChartContainer>
-        <FlexBox className="mt-[40px] w-full" direction="col">
-          <Text className="mb-[20px]  font-semibold">Feed Price Summary</Text>
-          <FlexBox className="w-full" justify="around">
-            <div className="w-full rounded-[4px] border border-neutral-200 p-[10px]">
-              <Text className="text-[14px] font-medium">Lowest</Text>
-              <Text className="text-[16px] text-success-500">₦680/kg</Text>
-            </div>
-            <div className="w-full rounded-[4px] border border-neutral-200 p-[10px]">
-              <Text className="text-[14px] font-medium">Average</Text>
-              <Text className="text-[16px] text-black">₦750/kg</Text>
-            </div>
-            <div className="w-full rounded-[4px] border border-neutral-200 p-[10px]">
-              <Text className="text-[14px] font-medium">Highest</Text>
-              <Text className="text-[16px] text-error-500">₦850/kg</Text>
-            </div>
-          </FlexBox>
+          </Bar>
+        </BarChart>
+      </ChartContainer>
+      <FlexBox direction="col" className="mb-[50px] mt-[30px] w-full">
+        <Text className="text-[14px] font-medium">Feed cost trends (₦/kg)</Text>
+        <FlexBox className="w-full" direction="row">
+          <div className="flex w-full flex-col gap-2 rounded-md border border-neutral-100 p-[10px]">
+            <Text className="font-medium leading-[20px]">Lowest</Text>
+            <Text className="text-[16px] font-semibold text-success-500">₦{feedPriceSummary?.lowestPrice}</Text>
+          </div>
+          <div className="flex w-full flex-col gap-2 rounded-md border border-neutral-100 p-[10px]">
+            <Text className="font-medium leading-[20px]">Average</Text>
+            <Text className="text-[16px] font-semibold">₦{feedPriceSummary?.averagePrice}</Text>
+          </div>
+          <div className="flex w-full flex-col gap-2 rounded-md border border-neutral-100 p-[10px]">
+            <Text className="font-medium leading-[20px]">Highest</Text>
+            <Text className="text-[16px] font-semibold text-error-500">₦{feedPriceSummary?.highestPrice}</Text>
+          </div>
         </FlexBox>
-      </CardContent>
+      </FlexBox>
     </Card>
   )
 }

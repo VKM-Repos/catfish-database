6591694ed9d2d02ae6e441cf8f861430db88407d
salissadiq <salissadiq@gmail.com>
2025-06-30 14:31:20 +0100

    fix(sales-records-columns): update cell accessors to use optional chaining for safer data retrieval

--- src/pages/dashboard/inventory/tabs/inventory/sales-records/columns.tsx
+++ src/pages/dashboard/inventory/tabs/inventory/sales-records/columns.tsx
@@ -7,36 +7,40 @@ export const columns: ColumnDef<any>[] = [
   {
     accessorKey: 'updatedAt',
     header: 'Date',
-    cell: ({ row }) => <Text weight="light">{'-'}</Text>,
+    cell: ({ row }) => {
+      console.log(row?.original)
+
+      return <Text weight="light">{'-'}</Text>
+    },
     // cell: ({ row }) => <Text weight="light">{formatDate(row.original.updatedAt ?? '') || '-'}</Text>,
   },
   {
     accessorKey: 'pond',
     header: 'Pond name',
-    cell: ({ row }) => <Text weight="light">{row.original.pond.name || '-'}</Text>,
+    cell: ({ row }) => <Text weight="light">{row?.original?.fishBatch?.pond?.name || '-'}</Text>,
   },
   {
     accessorKey: 'totalWeightHarvested',
     header: 'Total weight (kg)',
-    cell: ({ row }) => <Text weight="light">{row.original.totalWeightHarvested ?? '-'}</Text>,
+    cell: ({ row }) => <Text weight="light">{row?.original?.totalWeightHarvested ?? '-'}</Text>,
   },
   {
     accessorKey: 'quantity',
     header: 'No. of fish sold',
-    cell: ({ row }) => <Text weight="light">{row.original.quantity ?? 0}</Text>,
+    cell: ({ row }) => <Text weight="light">{row?.original?.quantity ?? 0}</Text>,
   },
   {
     accessorKey: 'costPerKg',
     header: 'Cost price per kg (₦)',
     cell: ({ row }) => (
-      <Text weight="light">{row.original.costPerKg < 1 ? '-' : formatPrice(row.original.costPerKg)}</Text>
+      <Text weight="light">{row?.original?.costPerKg < 1 ? '-' : formatPrice(row?.original?.costPerKg)}</Text>
     ),
   },
   {
     accessorKey: 'costPerFish',
     header: 'Total cost (₦)',
     cell: ({ row }) => (
-      <Text weight="light">{row.original.costPerFish < 1 ? '-' : formatPrice(row.original.costPerFish)}</Text>
+      <Text weight="light">{row?.original?.costPerFish < 1 ? '-' : formatPrice(row?.original?.costPerFish)}</Text>
     ),
   },
   {

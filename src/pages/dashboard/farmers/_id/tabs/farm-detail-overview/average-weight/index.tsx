f8c3d5b671a9d4d980463b8fd036f62b340726c7
Laouali <laoualibachir2000@gmail.com>
2025-07-16 14:51:21 +0100

    feat(farm-detail-overview): enhance data fetching for farm statistics and charts

--- src/pages/dashboard/farmers/_id/tabs/farm-detail-overview/average-weight/index.tsx
+++ src/pages/dashboard/farmers/_id/tabs/farm-detail-overview/average-weight/index.tsx
@@ -5,6 +5,9 @@ import { ChartHeader } from 'src/components/global/chart-header'
 import { Interval, IntervalFilter } from 'src/components/ui/interval-filter'
 import { DateRange } from 'src/components/ui/mega-datepicker'
 import { useState } from 'react'
+import { createGetQueryHook } from 'src/api/hooks/useGet'
+import { averageWeightSchema } from 'src/schemas/clusterFarmerSchema'
+import { Text } from 'src/components/ui/text'
 
 const averageWeightConfig = {
   averageWeight: {
@@ -15,10 +18,29 @@ const averageWeightConfig = {
 
 interface AverageWeightProps {
   dateRange?: DateRange
+  farmerId?: string
 }
 
-export default function AverageWeight({ dateRange }: AverageWeightProps) {
+export default function AverageWeight({ dateRange, farmerId }: AverageWeightProps) {
   const [interval, setInterval] = useState<Interval>('MONTHLY')
+
+  const useGetAverageWeight = createGetQueryHook({
+    endpoint: '/dashboards/cluster/volume-of-sales',
+    responseSchema: averageWeightSchema,
+    queryKey: ['average-weight'],
+  })
+
+  const { data: averageWeight, isLoading: averWeigIsLoading } = useGetAverageWeight({
+    query: {
+      interval,
+      farmerId: farmerId,
+      startDate: dateRange?.from?.toISOString().split('T')[0],
+      endDate: dateRange?.to?.toISOString().split('T')[0],
+    },
+  })
+
+  const hasNoData = (averageWeight?.length ?? 0) === 0
+
   return (
     <Card className="h-[30rem] w-full rounded-[.875rem] border border-neutral-200 p-4 md:w-[60%]">
       <ChartHeader
@@ -30,38 +52,54 @@ export default function AverageWeight({ dateRange }: AverageWeightProps) {
         }
       />
       <ChartContainer config={averageWeightConfig}>
-        <AreaChart
-          accessibilityLayer
-          data={averageWeightData}
-          margin={{
-            left: 12,
-            right: 12,
-            top: 12,
-            bottom: 12,
-          }}
-        >
-          <CartesianGrid vertical={false} strokeDasharray="10 10" />
-          <XAxis
-            dataKey="month"
-            tick={{ fill: '#737780', fontSize: 10 }}
-            tickLine={false}
-            axisLine={false}
-            tickMargin={8}
-            tickFormatter={(value) => value.slice(0, 3)}
-          />
-          <YAxis tick={{ fill: '#737780', fontSize: 10 }} axisLine={false} tickLine={false} tickMargin={4} width={20} />
-          <defs>
-            <linearGradient id="colorWeight" x1="0" y1="0" x2="0" y2="1">
-              <stop offset="0%" stopColor="#651391" stopOpacity={0.7} />
-              <stop offset="100%" stopColor="#651391" stopOpacity={0} />
-            </linearGradient>
-          </defs>
-          <ChartTooltip
-            cursor={false}
-            content={<ChartTooltipContent indicator="dot" hideLabel={false} className="!min-w-[10rem] bg-white" />}
-          />
-          <Area dataKey="averageWeight" type="linear" stroke="#651391" fill="url(#colorWeight)" />
-        </AreaChart>
+        {averWeigIsLoading ? (
+          <div className="flex h-full items-center justify-center">
+            <Text className="text-sm text-neutral-400">Loading chart...</Text>
+          </div>
+        ) : hasNoData ? (
+          <div className="flex h-full items-center justify-center">
+            <Text className="text-sm text-neutral-500">No data available for this filter</Text>
+          </div>
+        ) : (
+          <AreaChart
+            accessibilityLayer
+            data={averageWeight}
+            margin={{
+              left: 12,
+              right: 12,
+              top: 12,
+              bottom: 12,
+            }}
+          >
+            <CartesianGrid vertical={false} strokeDasharray="10 10" />
+            <XAxis
+              dataKey="intervalLabel"
+              tick={{ fill: '#737780', fontSize: 10 }}
+              tickLine={false}
+              axisLine={false}
+              tickMargin={8}
+              // tickFormatter={(value) => value.slice(0, 3)}
+            />
+            <YAxis
+              tick={{ fill: '#737780', fontSize: 10 }}
+              axisLine={false}
+              tickLine={false}
+              tickMargin={4}
+              width={20}
+            />
+            <defs>
+              <linearGradient id="colorWeight" x1="0" y1="0" x2="0" y2="1">
+                <stop offset="0%" stopColor="#651391" stopOpacity={0.7} />
+                <stop offset="100%" stopColor="#651391" stopOpacity={0} />
+              </linearGradient>
+            </defs>
+            <ChartTooltip
+              cursor={false}
+              content={<ChartTooltipContent indicator="dot" hideLabel={false} className="!min-w-[10rem] bg-white" />}
+            />
+            <Area dataKey="averageFishWeight" type="linear" stroke="#651391" fill="url(#colorWeight)" />
+          </AreaChart>
+        )}
       </ChartContainer>
     </Card>
   )

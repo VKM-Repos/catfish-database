6d47c5528269c86d0d434611bb347b3aa581efb1
salissadiq <salissadiq@gmail.com>
2025-07-20 22:13:43 +0100

    feat(cluster-manager): add feeding insight component with interval filtering

---
+++ src/pages/dashboard/home/overview/components/cluster-manager/feeding-insight.tsx
@@ -0,0 +1,132 @@
+import { useState } from 'react'
+import { Bar, BarChart, CartesianGrid, XAxis, YAxis } from 'recharts'
+import { createGetQueryHook } from 'src/api/hooks/useGet'
+import { ChartHeader } from 'src/components/global/chart-header'
+import { Button } from 'src/components/ui/button'
+import { Card } from 'src/components/ui/card'
+import { ChartContainer, ChartTooltip, ChartTooltipContent } from 'src/components/ui/chart'
+import { Popover, PopoverContent, PopoverTrigger } from 'src/components/ui/popover'
+import { Text } from 'src/components/ui/text'
+import { z } from 'zod'
+import * as SolarIconSet from 'solar-icon-set'
+
+const monthlyFeedConfig = {
+  monthlyFeed: {
+    label: '',
+    color: '#651391B2',
+  },
+}
+type Interval = 'DAILY' | 'WEEKLY' | 'MONTHLY' | 'YEARLY' | 'ALL'
+type DateRange = { from: Date; to: Date }
+
+function SelectPopover<T extends string>({
+  label,
+  options,
+  value,
+  onChange,
+}: {
+  label: string
+  options: T[]
+  value: T
+  onChange: (newVal: T) => void
+}) {
+  const [open, setOpen] = useState(false)
+  return (
+    <Popover open={open} onOpenChange={setOpen}>
+      <PopoverTrigger asChild>
+        <Button variant="neutral" size="sm" className="flex gap-x-2 !border border-neutral-200 bg-white capitalize">
+          {label} <SolarIconSet.AltArrowDown size={14} />
+        </Button>
+      </PopoverTrigger>
+
+      <PopoverContent className="flex max-h-64 w-40 flex-col gap-1 overflow-y-scroll p-2">
+        {options.map((opt) => (
+          <Button
+            key={opt}
+            variant={opt === value ? 'neutral' : 'ghost'}
+            size="sm"
+            className="justify-start capitalize"
+            onClick={() => {
+              onChange(opt)
+              setOpen(false)
+            }}
+          >
+            {opt.replaceAll('_', ' ').toLowerCase()}
+          </Button>
+        ))}
+      </PopoverContent>
+    </Popover>
+  )
+}
+function IntervalFilter({ value, onChange }: { value: Interval; onChange: (v: Interval) => void }) {
+  const options: Interval[] = ['DAILY', 'WEEKLY', 'MONTHLY', 'YEARLY', 'ALL']
+  return <SelectPopover<Interval> label={value.toLowerCase()} options={options} value={value} onChange={onChange} />
+}
+interface FeedingInsightProps {
+  dateRange?: DateRange
+}
+export default function FeedingInsight({ dateRange }: FeedingInsightProps) {
+  const [selectedFeedType, setSelectedFeedType] = useState<string | null>(null)
+  const [interval, setInterval] = useState<Interval>('MONTHLY')
+
+  const useGetFeedConsumed = createGetQueryHook({
+    endpoint: '/dashboards/cluster/feed-consumption-trend',
+    responseSchema: z.any(),
+    queryKey: ['most-feed-consumed-trend-cluster-manager'],
+  })
+  const { data: feedConsumed } = useGetFeedConsumed({
+    query: {
+      interval,
+      startDate: dateRange?.from?.toISOString().split('T')[0],
+      endDate: dateRange?.to?.toISOString().split('T')[0],
+    },
+  })
+
+  const restructuredData = feedConsumed?.map((item: any) => ({
+    feedType: item.feedType,
+    quantityKg: item.dataPoints[0].quantityInKg,
+  }))
+
+  return (
+    <Card className="w-full rounded-[.875rem] border border-neutral-200 p-4">
+      <div className="flex">
+        <ChartHeader
+          title={`${interval.charAt(0).toUpperCase()}${interval.slice(1).toLowerCase()} 'Feed Insights`}
+          action={<IntervalFilter value={interval} onChange={setInterval} />}
+        />
+      </div>
+      <Text className="text-[14px] font-medium">Most used feed brands (kg)</Text>
+      <ChartContainer className="-ml-5 mt-[10px] w-full" config={monthlyFeedConfig}>
+        <BarChart accessibilityLayer data={restructuredData} height={100} barCategoryGap={20}>
+          <CartesianGrid />
+          <XAxis dataKey="feedType" tickLine={false} tickMargin={10} axisLine={false} />
+          <YAxis tick={{ fill: '#737780', fontSize: 10 }} axisLine={false} tickLine={false} tickMargin={4} width={90} />
+          <ChartTooltip
+            cursor={false}
+            content={<ChartTooltipContent indicator="dot" hideLabel={false} className="!min-w-[10rem] bg-white !p-2" />}
+          />
+          <Bar dataKey="quantityKg" fill="#651391" radius={5} />
+        </BarChart>
+      </ChartContainer>
+    </Card>
+  )
+}
+
+const monthlyFeedData = [
+  {
+    feedType: 'Cppens',
+    feedQuantity: '50',
+  },
+  {
+    feedType: 'Vital',
+    feedQuantity: '40',
+  },
+  {
+    feedType: 'Topfeed',
+    feedQuantity: '30',
+  },
+  {
+    feedType: 'Blue Crown',
+    feedQuantity: '20',
+  },
+]

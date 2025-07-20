ce8ad74f7b5e407b98d6ffbaf5887492ce3731d7
salissadiq <salissadiq@gmail.com>
2025-07-20 22:30:11 +0100

    feat(harvest-volume-trends): add interval filter and dynamic data fetching

--- src/pages/dashboard/home/overview/components/super-admin/harvest-volume-trends.tsx
+++ src/pages/dashboard/home/overview/components/super-admin/harvest-volume-trends.tsx
@@ -1,20 +1,14 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { CartesianGrid, Line, LineChart, XAxis, YAxis } from 'recharts'
 import { createGetQueryHook } from 'src/api/hooks/useGet'
 import { Card, CardContent } from 'src/components/ui/card'
 import { ChartConfig, ChartContainer, ChartTooltip, ChartTooltipContent } from 'src/components/ui/chart'
-import { Text } from 'src/components/ui/text'
 import { z } from 'zod'
-
-const chartData = [
-  { month: 'Q1', value: 5, mobile: 80 },
-  { month: 'Q2', value: 20, mobile: 80 },
-  { month: 'Q3', value: 30, mobile: 200 },
-  { month: 'Q4', value: 10, mobile: 120 },
-  { month: 'Q5', value: 60, mobile: 190 },
-  { month: 'Q6', value: 70, mobile: 130 },
-  { month: 'Q7', value: 90, mobile: 140 },
-]
+import * as SolarIconSet from 'solar-icon-set'
+import { Popover } from '@radix-ui/react-popover'
+import { PopoverContent, PopoverTrigger } from 'src/components/ui/popover'
+import { Button } from 'src/components/ui/button'
+import { ChartHeader } from 'src/components/global/chart-header'
 
 const chartConfig = {
   value: {
@@ -26,20 +20,82 @@ const chartConfig = {
     color: '#0DA500',
   },
 } satisfies ChartConfig
-export default function HarvestVolumeTrends() {
+
+type Interval = 'DAILY' | 'WEEKLY' | 'MONTHLY' | 'YEARLY' | 'ALL'
+type DateRange = { from: Date; to: Date }
+
+function SelectPopover<T extends string>({
+  label,
+  options,
+  value,
+  onChange,
+}: {
+  label: string
+  options: T[]
+  value: T
+  onChange: (newVal: T) => void
+}) {
+  const [open, setOpen] = useState(false)
+  return (
+    <Popover open={open} onOpenChange={setOpen}>
+      <PopoverTrigger asChild>
+        <Button variant="neutral" size="sm" className="flex gap-x-2 !border border-neutral-200 bg-white capitalize">
+          {label} <SolarIconSet.AltArrowDown size={14} />
+        </Button>
+      </PopoverTrigger>
+
+      <PopoverContent className="flex max-h-64 w-40 flex-col gap-1 overflow-y-scroll p-2">
+        {options.map((opt) => (
+          <Button
+            key={opt}
+            variant={opt === value ? 'neutral' : 'ghost'}
+            size="sm"
+            className="justify-start capitalize"
+            onClick={() => {
+              onChange(opt)
+              setOpen(false)
+            }}
+          >
+            {opt.replaceAll('_', ' ').toLowerCase()}
+          </Button>
+        ))}
+      </PopoverContent>
+    </Popover>
+  )
+}
+function IntervalFilter({ value, onChange }: { value: Interval; onChange: (v: Interval) => void }) {
+  const options: Interval[] = ['DAILY', 'WEEKLY', 'MONTHLY', 'YEARLY', 'ALL']
+  return <SelectPopover<Interval> label={value.toLowerCase()} options={options} value={value} onChange={onChange} />
+}
+interface HarvestVolumeTrendsProps {
+  dateRange?: DateRange
+}
+
+export default function HarvestVolumeTrends({ dateRange }: HarvestVolumeTrendsProps) {
+  const [interval, setInterval] = useState<Interval>('MONTHLY')
+
   const useGetRevenuePrice = createGetQueryHook({
-    endpoint: '/dashboards/super-admin/volume-of-sales?interval=MONTHLY',
+    endpoint: '/dashboards/super-admin/volume-of-sales',
     responseSchema: z.any(),
     queryKey: ['sales-revenue-price-trends-super-admin'],
   })
-  const { data: salesRevenue } = useGetRevenuePrice()
-  console.log(salesRevenue)
+  const { data: salesRevenue } = useGetRevenuePrice({
+    query: {
+      interval,
+      startDate: dateRange?.from?.toISOString().split('T')[0],
+      endDate: dateRange?.to?.toISOString().split('T')[0],
+    },
+  })
 
   return (
     <Card className="h-[450px] max-h-[450px] min-h-[450px] w-full border border-neutral-200 p-[16px] ">
       <CardContent>
-        <Text className="mb-[20px]  font-semibold">Harvest Volume Trend </Text>
-
+        <div className="flex">
+          <ChartHeader
+            title={`${interval.charAt(0).toUpperCase()}${interval.slice(1).toLowerCase()} 'Harvest Volume Trend'`}
+            action={<IntervalFilter value={interval} onChange={setInterval} />}
+          />
+        </div>
         <ChartContainer config={chartConfig}>
           <LineChart
             accessibilityLayer

edc382fe7957980074014d289ed45e3e92a454a6
salissadiq <salissadiq@gmail.com>
2025-07-24 20:39:37 +0100

    feat(subdomain-config): add configuration for various subdomains with API base URLs

---
+++ src/api/config/subdomains.ts
@@ -0,0 +1,25 @@
+const subdomainConfig: any = {
+  'ajegunle-test-cluster': {
+    apiBaseUrl: 'https://ajegunle-test-cluster.aquadata.com.ng/api',
+  },
+  'camp74-test-cluster': {
+    apiBaseUrl: 'https://camp74-test-cluster.aquadata.com.ng/api',
+  },
+  dev: {
+    apiBaseUrl: 'https://dev.aquadata.com.ng/api',
+  },
+  'eriwe-test-cluster': {
+    apiBaseUrl: 'https://eriwe-test-cluster.aquadata.com.ng/api',
+  },
+  'kumbotso-test-cluster': {
+    apiBaseUrl: 'https://kumbotso-test-cluster.aquadata.com.ng/api',
+  },
+  'waziri-test-cluster': {
+    apiBaseUrl: 'https://waziri-test-cluster.aquadata.com.ng/api',
+  },
+  default: {
+    apiBaseUrl: 'https://dev.aquadata.com.ng/api',
+  },
+}
+
+export default subdomainConfig

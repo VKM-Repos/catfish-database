5e985f02746adc17ccbf9880b63db52297663653
Laouali <laoualibachir2000@gmail.com>
2025-07-30 11:37:55 +0100

    chore(css): fix the style of fcr overall

--- src/pages/dashboard/home/overview/components/farmer/fcr.overall.tsx
+++ src/pages/dashboard/home/overview/components/farmer/fcr.overall.tsx
@@ -23,27 +23,27 @@ export default function FcrOverall({ dateRange }: FcrOverallProps) {
     },
   })
   return (
-    <Card className="flex h-[400px] max-h-[400px] min-h-[400px] w-full items-center p-[24px]">
-      <div className="flex w-full flex-col">
-        <CardContent>
-          <FlexBox className="mt-[20px] w-full" direction="col">
-            <FlexBox className="mt-[20px] w-full" direction="row">
-              <div className="flex w-full flex-col gap-2 rounded-md border border-neutral-100 p-[10px]">
-                <Text className="font-medium leading-[20px]">Feed Conversion Ratio</Text>
-                <Text className="text-[16px] font-semibold">{fcr ? fcr[0]?.fcrValue : 0}</Text>
-                <Text className="text-[10px] font-medium text-success-500">Good</Text>
+    <Card className="flex h-[400px] max-h-[400px] min-h-[400px] w-full">
+      <div className="flex h-full w-full flex-col py-5">
+        <CardContent className="flex-1">
+          <FlexBox className="my-[20px] h-full w-full" direction="col">
+            <FlexBox className="  w-full flex-1" direction="row">
+              <div className="flex h-full w-full flex-col justify-center gap-2 rounded-md border-2 border-neutral-100 p-[10px]">
+                <Text className="text-lg font-medium leading-[20px]">Feed Conversion Ratio</Text>
+                <Text className="text-xl font-semibold">{fcr ? fcr[0]?.fcrValue : 0}</Text>
+                <Text className="text-sm font-medium text-success-500">Good</Text>
               </div>
-              <div className="flex w-full flex-col gap-2 rounded-md border border-neutral-100 p-[10px]">
-                <Text className="font-medium leading-[20px]">Feed Cost</Text>
-                <Text className="text-[16px] font-semibold">₦{fcr ? fcr[0]?.totalWeightGained : 0}</Text>
-                <Text className="text-[10px] font-medium text-[#000AFF]">₦5.5 per kg</Text>
+              <div className="flex h-full w-full flex-col justify-center gap-2 rounded-md border-2 border-neutral-100 p-[10px]">
+                <Text className="text-lg font-medium leading-[20px]">Feed Cost</Text>
+                <Text className="text-xl font-semibold">₦{fcr ? fcr[0]?.totalWeightGained : 0}</Text>
+                <Text className="text-sm font-medium text-[#000AFF]">₦5.5 per kg</Text>
               </div>
             </FlexBox>
 
-            <div className="flex w-full flex-col gap-2 rounded-md border border-neutral-100 p-[10px]">
-              <Text className="font-medium leading-[20px]">Total Feed Consumed</Text>
-              <Text className="text-[16px] font-semibold">{fcr ? fcr[0]?.totalFeedConsumed : 0} kg</Text>
-              <Text className="text-[10px] font-medium">For current production cycle</Text>
+            <div className="flex w-full flex-1 flex-col justify-center gap-2 rounded-md border-2 border-neutral-100 p-[10px]">
+              <Text className="text-lg font-medium leading-[20px]">Total Feed Consumed</Text>
+              <Text className="text-xl font-semibold">{fcr ? fcr[0]?.totalFeedConsumed : 0} kg</Text>
+              <Text className="text-sm font-medium">For current production cycle</Text>
             </div>
           </FlexBox>
         </CardContent>

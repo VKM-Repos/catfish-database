37d6b920c109ddfdb6409df027cdb7461e7e5bed
ChimarokeAmadi <chirokeamadi@gmail.com>
2025-08-02 10:49:46 +0100

    fix(feed-stock-form):  added z-index to SelectContent and datePicker components for better display

--- src/pages/dashboard/inventory/create/feed-stock/feed-stock-form/index.tsx
+++ src/pages/dashboard/inventory/create/feed-stock/feed-stock-form/index.tsx
@@ -208,7 +208,7 @@ export default function FeedStockForm({
                               <SelectValue placeholder="Select Feed Type" />
                             </div>
                           </SelectTrigger>
-                          <SelectContent>
+                          <SelectContent className="z-[82]">
                             {Object.values(AvailableFeedTypes).map((type) => {
                               // Format: remove underscores, capitalize each word
                               const label = type
@@ -257,7 +257,7 @@ export default function FeedStockForm({
                               <SelectValue placeholder="Select Pellet size" />
                             </div>
                           </SelectTrigger>
-                          <SelectContent>
+                          <SelectContent className="z-[82]">
                             {PelletSizes?.map((pellet) => (
                               <SelectItem key={pellet} value={parseFloat(pellet).toString()}>
                                 {pellet}

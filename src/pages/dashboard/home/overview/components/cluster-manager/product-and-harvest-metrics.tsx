f308543b9b2a72a8a725468e8b53ffc3d475d440
salissadiq <salissadiq@gmail.com>
2025-08-03 21:54:39 +0100

    fix(product-and-harvest-metrics): handle null values in total harvest volume display

--- src/pages/dashboard/home/overview/components/cluster-manager/product-and-harvest-metrics.tsx
+++ src/pages/dashboard/home/overview/components/cluster-manager/product-and-harvest-metrics.tsx
@@ -22,52 +22,54 @@ export default function ProductAndHarvestMetrics({ dateRange }: ProductAndHarves
 
   return (
     <Card className="border border-neutral-200 p-[24px]">
-      <FlexBox>
-        <Card className="w-full p-[10px]">
-          <FlexBox direction="col" gap="gap-1">
-            <Text size="sm" weight="medium">
-              Total harvest volume
-            </Text>
-            <Text size="lg" weight="semibold">
-              {salesVolume && formatNumber(salesVolume[0]?.totalWeight)} kg
-            </Text>
-            <Text size="xs">YTD</Text>
-          </FlexBox>
-        </Card>
-        <Card className="w-full p-[10px]">
-          <FlexBox direction="col" gap="gap-1">
-            <Text size="sm" weight="medium">
-              Average selling price
-            </Text>
-            <Text size="lg" weight="semibold">
-              ₦{salesVolume && salesVolume[0]?.averageSellingPrice}
-            </Text>
-            <Text size="xs">Current</Text>
-          </FlexBox>
-        </Card>
-        <Card className="w-full p-[10px]">
-          <FlexBox direction="col" gap="gap-1">
-            <Text size="sm" weight="medium">
-              Total revenue
-            </Text>
-            <Text size="lg" weight="semibold">
-              ₦{salesVolume && salesVolume[0]?.totalRevenue}
-            </Text>
-            <Text size="xs">YTD</Text>
-          </FlexBox>
-        </Card>
-        <Card className="w-full p-[10px]">
-          <FlexBox direction="col" gap="gap-1">
-            <Text size="sm" weight="medium">
-              Average fish weight
-            </Text>
-            <Text size="lg" weight="semibold">
-              {salesVolume && salesVolume[0]?.averageFishWeight} kg
-            </Text>
-            <Text size="xs">Current</Text>
-          </FlexBox>
-        </Card>
-      </FlexBox>
+      {
+        <FlexBox>
+          <Card className="w-full p-[10px]">
+            <FlexBox direction="col" gap="gap-1">
+              <Text size="sm" weight="medium">
+                Total harvest volume
+              </Text>
+              <Text size="lg" weight="semibold">
+                {salesVolume && formatNumber(salesVolume[0]?.totalWeight !== null ? salesVolume[0]?.totalWeight : 0)} kg
+              </Text>
+              <Text size="xs">YTD</Text>
+            </FlexBox>
+          </Card>
+          <Card className="w-full p-[10px]">
+            <FlexBox direction="col" gap="gap-1">
+              <Text size="sm" weight="medium">
+                Average selling price
+              </Text>
+              <Text size="lg" weight="semibold">
+                ₦{salesVolume && salesVolume[0]?.averageSellingPrice}
+              </Text>
+              <Text size="xs">Current</Text>
+            </FlexBox>
+          </Card>
+          <Card className="w-full p-[10px]">
+            <FlexBox direction="col" gap="gap-1">
+              <Text size="sm" weight="medium">
+                Total revenue
+              </Text>
+              <Text size="lg" weight="semibold">
+                ₦{salesVolume && salesVolume[0]?.totalRevenue}
+              </Text>
+              <Text size="xs">YTD</Text>
+            </FlexBox>
+          </Card>
+          <Card className="w-full p-[10px]">
+            <FlexBox direction="col" gap="gap-1">
+              <Text size="sm" weight="medium">
+                Average fish weight
+              </Text>
+              <Text size="lg" weight="semibold">
+                {salesVolume && salesVolume[0]?.averageFishWeight} kg
+              </Text>
+              <Text size="xs">Current</Text>
+            </FlexBox>
+          </Card>
+        </FlexBox>
+      }
       <FlexBox>
         <HarvestVolumeOvertime dateRange={dateRange} />
         <LineChartHarvestVolume dateRange={dateRange} />

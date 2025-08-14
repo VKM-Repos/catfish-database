605e7f85c2377d09704ef8bd8ead9e01f2bc93ef
salissadiq <salissadiq@gmail.com>
2025-08-14 15:31:01 +0100

    feat(feeding-report): add pond name column to mortality report table

--- src/pages/dashboard/reports/tabs/reports-table/feeding-report/mortality-column.tsx
+++ src/pages/dashboard/reports/tabs/reports-table/feeding-report/mortality-column.tsx
@@ -15,6 +15,11 @@ export const mortalityColumn: ColumnDef<any>[] = [
     cell: ({ row }) => <Text weight="light">{extractTimeFromISO(row.original.time)}</Text>,
   },
   {
+    accessorKey: 'batch.pond.name',
+    header: 'Pond',
+    cell: ({ row }) => <Text weight="light">{row.original.batch.pond.name || '-'}</Text>,
+  },
+  {
     accessorKey: 'mortalityNumber',
     header: 'Mortality number',
     cell: ({ row }) => <Text weight="light">{row.original?.mortalityNumber ?? '-'}</Text>,
